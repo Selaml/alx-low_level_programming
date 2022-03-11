0e3f7f32024c3e7a4801f876a10260bbb8cc1776
selaml <selamawitlb@gmail.com>
2022-03-11 05:28:52 -0800

    lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,7 +8,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	putchar(tolower('A'));
+{ 
+	putchar(tolower('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'));
+	putchar(tolower('N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'));
 	return (0);
 }
