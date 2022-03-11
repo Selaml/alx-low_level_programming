b4b1540d1970f2a2d7d26170a14224f36a0ce801
selaml <selamawitlb@gmail.com>
2022-03-11 10:48:33 -0800

    lower and uppercase letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,7 @@ int main(void)
 	char letter, letter1;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	for (letter1 = 'A'; letter1 <= 'Z'; letter++)
+	for (letter1 = 'A'; letter1 <= 'Z'; letter1++)
 		putchar(letter);
 		putchar(letter1);
 
