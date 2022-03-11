f3aea1be1df02561cc121e6d222cbbf49f6f1598
selaml <selamawitlb@gmail.com>
2022-03-11 11:09:05 -0800

    print single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		printf(number);
+		printf('\n);
+	}
+	return (0);
+}
