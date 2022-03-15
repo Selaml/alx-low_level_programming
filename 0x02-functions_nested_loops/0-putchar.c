ff0022a220de937a10d052eebdccf82a9f6b7ca9
selaml <selamawitlb@gmail.com>
2022-03-15 04:51:33 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print all possible combinations of single-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar\n")
+
+	return (0);
+}
