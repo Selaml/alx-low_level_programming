7a5ad6c724b74e1bbf53dd2153524cfd80bc0450
selaml <selamawitlb@gmail.com>
2022-03-17 12:08:10 -0700

    prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_numbers- Check if a character is a digit
+  * 
+  *
+  * Return: prints the numbers, from 0 to 9
+  */
+void print_numbers(void)
+{
+	int n;
+
+	for (n >= 48 && x <= 57)
+	
+		_putchar(n);
+		_putchar('\n');
+
+
+	return (0);
+}
