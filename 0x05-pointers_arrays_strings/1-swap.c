d665b0f8ddb5715b6f6f8ccd6a455f7a56089c63
selaml <selamawitlb@gmail.com>
2022-03-22 00:01:39 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int and updates
+ *               the value it points to to 98.
+ * @n: The pointer to an int.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
