6698572237856c59db1829e6cf6edce61a64f2f1
selaml <selamawitlb@gmail.com>
2022-03-31 11:56:55 -0700

    print length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- * factorial - Returns the factorial of a given number.
- * @n: The number to find the factorial of.
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
  *
- * Return: If n > 0 - the factorial of n.
- *         If n < 0 - 1 to indicate an error.
+ * Return: The length of the string.
  */
-int factorial(int n)
+int _strlen_recursion(char *s)
 {
-	int result = n;
+	int len = 0;
 
-	if (n < 0)
-		return (-1);
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
 
-	else if (n >= 0 && n <= 1)
-		return (1);
-
-	result *= factorial(n - 1);
-
-	return (result);
+	return (len);
 }
