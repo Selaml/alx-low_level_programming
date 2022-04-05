c852e0d40a9e85c1a3295b0a9f5d7114bf7b61d6
selaml <selamawitlb@gmail.com>
2022-04-04 23:39:11 -0700

    static

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -4,19 +4,12 @@
 /**
  * main - entry point
  *
+ * @c: inpur character
  * Description: Print _putchar
  * Return: 0
  */
 int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	_putchar(c);
 	return (0);
 }
